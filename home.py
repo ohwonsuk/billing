39913134e67d0e80b7da7c28cec91409b0b6f57b
ohwonsuk <wsoh51@gmail.com>
2024-04-02 08:59:51 +0900

    엑셀 다운로드 폴더 지정

--- home.py
+++ home.py
@@ -285,7 +285,7 @@ if customer_file is not None:
             ws2['M'+str(i)].border = border
             ws2['I'+str(i)].number_format = '#,##0'
 
-        wb.save(f'{customer_name}_{month}월_스마트링크내역서.xlsx')
+        wb.save(f'/Users/wonsuk/Data/output/{customer_name}_{month}월_스마트링크내역서.xlsx')
         st.write('청구내역서 생성완료')
 
 else:
